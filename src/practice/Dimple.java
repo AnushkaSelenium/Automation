b2f491d2637fdce7380a719aaa5388ab030ae01b
hp <hp@DESKTOP-TC87MBS>
2024-01-29 09:40:21 +0530

    Tammudu its my first program on github

---
+++ src/practice/Dimple.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Dimple {
+@Test
+public void m3() {
+	Reporter.log("Hello..",true);
+}
+}
---
+++ src/practice/Sample.java
@@ -0,0 +1,17 @@
+package practice;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample {
+@Test
+public void m1() {
+	Reporter.log("Hii Tammudu",true);
+}
+@Test
+public void m2() {
+	Reporter.log("Lets do Kummudu",true);
+	Assert.fail();//Intentionally we are failing the TC
+}
+}
